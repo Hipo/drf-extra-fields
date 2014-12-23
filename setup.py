1c77d8e0082ebba0215565fc53489be10de26dd7
Umut Bozkurt <umut@hipo.biz>
2014-12-23 14:24:07 +0200

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-extra-fields',
-    version='0.1',
+    version='0.3',
     packages=['drf_extra_fields'],
     include_package_data=True,
     license=' License',  # example license
