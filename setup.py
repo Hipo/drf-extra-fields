969e8d880a5b042ff9d87ea3e317d895483bc352
vargi <tunavargi@gmail.com>
2015-11-03 15:55:14 +0300

    version updated

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-extra-fields',
-    version='0.3',
+    version='0.4',
     packages=['drf_extra_fields',
               'drf_extra_fields.runtests',
               ],
