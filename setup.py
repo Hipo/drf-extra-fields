89131439d4354fb0d587affa639edbdaa588cf30
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-10-28 18:43:41 +0300

    Bump Pillow version to 6.2.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     include_package_data=True,
     install_requires=['Django >= 1.4', 'djangorestframework >= 3.0.1'],
     extras_require={
-        "Base64ImageField": ["Pillow == 6.2.0"],
+        "Base64ImageField": ["Pillow == 6.2.1"],
     },
     license='Apache-2.0',
     description='Additional fields for Django Rest Framework.',
