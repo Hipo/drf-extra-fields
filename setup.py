5a4dfe441e1fbb9ac5922c4bc89e8d9034898078
Ali <alicertel@gmail.com>
2019-12-06 13:24:56 +0300

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-extra-fields',
-    version='2.0.1',
+    version='2.0.2',
     packages=['drf_extra_fields',
               'drf_extra_fields.runtests'],
     include_package_data=True,
@@ -39,6 +39,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
