23581e525db8548c29287ce4592f944ceef4a062
kadir can çetin <kadircancetin@gmail.com>
2020-08-17 17:06:08 +0300

    Add read_source parameter on PrimaryKeyRelatedField

--- drf_extra_fields/relations.py
+++ drf_extra_fields/relations.py
@@ -1,10 +1,26 @@
 from collections import OrderedDict
 
-from rest_framework.relations import PrimaryKeyRelatedField, SlugRelatedField
+from rest_framework.relations import (PrimaryKeyRelatedField, SlugRelatedField,
+                                      MANY_RELATION_KWARGS, ManyRelatedField)
+
+
+class CustomManyRelatedField(ManyRelatedField):
+    def __init__(self, child_relation=None, *args, **kwargs):
+        super().__init__(child_relation, *args, **kwargs)
+
+    def get_attribute(self, instance):
+        if self.child_relation.read_source:
+            return getattr(
+                self.child_relation.presentation_serializer.Meta.model,
+                self.child_relation.read_source
+            ).fget(instance)
+
+        return super().get_attribute(self, instance)
 
 
 class PresentableRelatedFieldMixin(object):
     def __init__(self, **kwargs):
+        self.read_source = kwargs.pop("read_source", None)
         self.presentation_serializer = kwargs.pop("presentation_serializer", None)
         self.presentation_serializer_kwargs = kwargs.pop(
             "presentation_serializer_kwargs", dict()
@@ -25,6 +41,15 @@ class PresentableRelatedFieldMixin(object):
         - https://github.com/tomchristie/django-rest-framework/blob/master/rest_framework/relations.py#L132
         """
         return False
+
+    @classmethod
+    def many_init(cls, *args, **kwargs):
+        list_kwargs = {'child_relation': cls(*args, **kwargs)}
+        for key in kwargs:
+            if key in MANY_RELATION_KWARGS:
+                list_kwargs[key] = kwargs[key]
+
+        return CustomManyRelatedField(**list_kwargs)
 
     def get_choices(self, cutoff=None):
         queryset = self.get_queryset()
