e171a27033553fc7d74adc1ba6cb1a9ed2ed1fb4
Ali <alicertel@gmail.com>
2020-11-04 18:59:16 +0300

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='drf-extra-fields',
-    version='3.0.3',
+    version='3.0.4',
     packages=['drf_extra_fields',
               'drf_extra_fields.runtests'],
     include_package_data=True,
