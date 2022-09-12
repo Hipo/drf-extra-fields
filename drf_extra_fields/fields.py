67a13b42d9256a595249284ea6805a627a3d8cc4
dfg-98 <dariofg98@gmail.com>
2022-09-12 18:10:25 -0400

    Add webp to ALLOWED_TYPES for Base64ImageField

--- drf_extra_fields/fields.py
+++ drf_extra_fields/fields.py
@@ -128,7 +128,8 @@ class Base64ImageField(Base64FieldMixin, ImageField):
         "jpeg",
         "jpg",
         "png",
-        "gif"
+        "gif",
+        "webp"
     )
     INVALID_FILE_MESSAGE = _("Please upload a valid image.")
     INVALID_TYPE_MESSAGE = _("The type of the image couldn't be determined.")
