295e2538b13c5d1ceace01022136ebe021d37996
Christian Hartung <christian.hartung@olist.com>
2023-05-30 12:27:49 -0300

    chore: add compatibility with Psycopg 3

--- drf_extra_fields/compat.py
+++ drf_extra_fields/compat.py
@@ -1,10 +1,19 @@
+import django
+
 try:
     from django.contrib.postgres.fields import FloatRangeField
 except ImportError:
     FloatRangeField = None
 
 try:
-    from psycopg2.extras import DateRange, DateTimeTZRange, NumericRange
+    if django.VERSION >= (4, 2):
+        try:
+            from psycopg.types.range import DateRange, NumericRange
+            from psycopg.types.range import TimestamptzRange as DateTimeTZRange
+        except ImportError:
+            from psycopg2.extras import DateRange, DateTimeTZRange, NumericRange
+    else:
+        from psycopg2.extras import DateRange, DateTimeTZRange, NumericRange
 except ImportError:
     DateRange = None
     DateTimeTZRange = None
